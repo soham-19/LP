dc1e463467dde0d5394461a02c047efc5212f4d5
SOham <114175483+soham-19@users.noreply.github.com>
2023-11-17 08:31:50 +0530

    Update IOT1IR.py

--- IOT1IR.py
+++ IOT1IR.py
@@ -1,11 +1,15 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
+
 GPIO.setup(33, GPIO.IN)
 GPIO.setup(38, GPIO.OUT)
+
 while True:
     i = GPIO.input(33)
+    
     if i==1 :
         print("OBSTACLE")
         GPIO.output(38, GPIO.HIGH)
